4a77582a5bfdeebb1af07e211da0be038975f1f9
mikesupertrampster <mikesupertrampster@gmail.com>
2022-04-29 14:58:39 +0700

    Sleep between addblocks.

--- simple/main.go
+++ simple/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/mikesupertrampster-corp/blockchain/simple/pkg/blockchain"
+import (
+	"github.com/mikesupertrampster-corp/blockchain/simple/pkg/blockchain"
+	"time"
+)
 
 func main() {
 	chain := blockchain.NewBlockChain()
 
 	chain.AddBlock("Send 1 BTC to Mike")
+	time.Sleep(3 * time.Second)
 	chain.AddBlock("Send 2 more BTC to Mike")
 
 	chain.Print()
